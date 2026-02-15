f356ecd682b75e8f424534d0d7eae3db52d5adb8
JoaquinF <joaquinpfelix@gmail.com>
2026-02-15 15:04:10 -0800

    Check 2

--- 210-Lab-6/210-Lab-6.cpp
+++ 210-Lab-6/210-Lab-6.cpp
@@ -1,16 +1,23 @@
 #include <iostream>
 #include <string>
 
+using namespace std;
 
 //Funtction Prototypes
-void fillArray(double* ptr, int size);
-void printArray(double* ptr, int size);
-double getTotal(double* ptr, int size);
+void enterArrayData(double* ptr, int size);
+void outputArrayData(double* ptr, int size);
+double sumArray(double* ptr, int size);
 
 int main() {
 
-    int size;
-    cout << "Enter array size";
+    const int size = 5;
+    double* numbers = new double[size];
+
+    cout << "Data entry for the array: " << endl;
+
+    enterArrayData(numbers, size);
+    outputArrayData(numbers, size);
+    cout << "Sum of values: " << sumArray(numbers, size) << endl;  
 
 
 }
