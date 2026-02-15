c27d2ea71cedf4ca4f7bb9f9ac0d328c8e88966a
JoaquinF <joaquinpfelix@gmail.com>
2026-02-15 14:47:55 -0800

    10 minute check I had to do some other things and that is the cause for the break between this and the initial commit

--- 210-Lab-6/210-Lab-6.cpp
+++ 210-Lab-6/210-Lab-6.cpp
@@ -1,9 +1,16 @@
 #include <iostream>
+#include <string>
 
 
-
-
+//Funtction Prototypes
+void fillArray(double* ptr, int size);
+void printArray(double* ptr, int size);
+double getTotal(double* ptr, int size);
 
 int main() {
+
+    int size;
+    cout << "Enter array size";
+
 
 }
